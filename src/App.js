1113d30c1c3efd1b83e9b7e7baa46959b83dbf45
Jaynti Kanani <jdkanani@gmail.com>
2019-05-19 12:05:10 +0530

    connect/disconnect

--- src/App.js
+++ src/App.js
@@ -4,32 +4,109 @@ import Web3 from "web3"
 
 import Header from "./components/header"
 
+let storage = null
+if (typeof window.localStorage !== "undefined") {
+  storage = window.localStorage
+}
+
 class App extends React.Component {
-  async connectToWallet() {
-    const maticProvider = new WalletConnectProvider({
-      host: "https://testnet2.matic.network"
+  constructor(props) {
+    super(props)
+    this.state = { address: null }
+    this.checkAuth()
+  }
+
+  checkAuth = () => {
+    if (storage && storage.getItem("loggedIn")) {
+      this.connectToWallet()
+    }
+  }
+
+  onConnect = () => {
+    if (storage) {
+      storage.setItem("loggedIn", true)
+    }
+  }
+
+  onDisconnect = () => {
+    this.setState({
+      address: null
     })
 
-    const web3 = new Web3(maticProvider)
-    web3.eth.getAccounts().then(accounts => {
-      console.log(accounts)
+    if (storage) {
+      storage.removeItem("loggedIn")
+    }
+  }
+
+  connectToWallet = () => {
+    const maticProvider = new WalletConnectProvider({
+      host: "https://testnet2.matic.network",
+      callbacks: {
+        onConnect: this.onConnect,
+        onDisconnect: this.onDisconnect
+      }
+    })
+
+    const ropstenProvider = new WalletConnectProvider({
+      host: "https://testnet2.matic.network",
+      callbacks: {
+        onConnect: this.onConnect,
+        onDisconnect: this.onDisconnect
+      }
+    })
+
+    const maticWeb3 = new Web3(maticProvider)
+    const ropstenWeb3 = new Web3(ropstenProvider)
+
+    // fetch accounts
+    maticWeb3.eth.getAccounts().then(accounts => {
+      // set address
+      this.setState({
+        maticWeb3,
+        ropstenWeb3,
+        maticProvider,
+        ropstenProvider,
+        address: accounts[0]
+      })
     })
   }
 
+  disconnect = () => {
+    const { maticProvider, ropstenProvider } = this.state
+    if (maticProvider) {
+      maticProvider.disconnect()
+    }
+
+    if (ropstenProvider) {
+      ropstenProvider.disconnect()
+    }
+  }
+
+  connectScreen() {
+    return (
+      <div className="screen-middle-container d-flex justify-content-center">
+        <div className="align-self-center">
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={this.connectToWallet}
+          >
+            Connect to Wallet
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  homeScreen() {
+    return <div className="container">Hello</div>
+  }
+
   render() {
+    const { address } = this.state
     return (
       <div>
-        <Header />
-        <div className="screen-middle-container d-flex justify-content-center">
-          <div className="align-self-center">
-            <button
-              className="btn btn-primary btn-lg"
-              onClick={this.connectToWallet}
-            >
-              Connect to Wallet
-            </button>
-          </div>
-        </div>
+        <Header address={address} disconnect={this.disconnect} />
+        {!address ? this.connectScreen() : this.homeScreen()}
       </div>
     )
   }
--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,27 @@ import React from "react"
 
 class Header extends React.Component {
   render() {
+    const { address, disconnect } = this.props
     return (
       <div className="navbar navbar-expand-lg navbar-light">
         <div id="navbarSupportedContent" className="collapse navbar-collapse">
           <ul className="navbar-nav justify-content-end ml-auto">
             <li className="nav-item">
-              <a className="nav-link nuxt-link-active" href="#">
-                Connect to Wallet
-              </a>
+              {address ? (
+                <div className="small">
+                  <div className="text-muted">{address}</div>
+                  <div
+                    className="cursor-pointer text-right"
+                    onClick={disconnect}
+                  >
+                    {address ? "Disconnect" : null}
+                  </div>
+                </div>
+              ) : (
+                <div className="nav-link text-muted small">
+                  No wallet connected
+                </div>
+              )}
             </li>
           </ul>
         </div>
